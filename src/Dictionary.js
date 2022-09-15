467603dbe8a5161b850f92a42b9fc3a1dfd32eac
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-09-15 17:53:41 +0100

    Added default word on load

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,17 +6,21 @@ import Results from "./Results";
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultWord);
   let [definition, setDefinition] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setDefinition(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     // Documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
@@ -24,22 +28,32 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <label className="label">What word do you want to look up?</label>
-        <form onSubmit={search}>
-          <input
-            type="search"
-            placeholder="sunset"
-            onChange={handleKeywordChange}
-            autoFocus="on"
-            className="input-group p-3 rounded border border-light"
-          ></input>
-        </form>
-        <p>i.e. paris, wine, yoga, coding</p>
-      </section>
-      <Results definition={definition} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <label className="label">What word do you want to look up?</label>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="sunset"
+              onChange={handleKeywordChange}
+              autoFocus="on"
+              className="input-group p-3 rounded border border-light"
+            ></input>
+          </form>
+          <p>i.e. paris, wine, yoga, coding</p>
+        </section>
+        <Results definition={definition} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,11 +3,13 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul>
-        {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
-        })}
-      </ul>
+      <div>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
     );
   } else {
     return null;
